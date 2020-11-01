0ce06a134775843e18aed4cbdd91dd8d7d8a8135
Christopher Scheideman <crscheideman@gmail.com>
2020-11-01 13:48:56 -0500

    updating challenge progress

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,4 +1,12 @@
+# Deliverable 1
 library(dbplyr)
-MechaCar_mpg <- read.csv(file = 'MechaCar_mpg.csv',check.names=F,stringsAsFactors=F) #import csv
+MechaCar_mpg <- read.csv(file = 'data/MechaCar_mpg.csv',check.names=F,stringsAsFactors=F) #import csv
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=MechaCar_mpg) #generate multiple linear regression model
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=MechaCar_mpg))
+
+# Deliverable 2
+Suspension_Coil <- read.csv('data/Suspension_Coil.csv',check.names = F,stringsAsFactors = F)
+total_summary <- Suspension_Coil %>%  
+  summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD = sd(PSI))
+lot_summary <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>%  
+  summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD = sd(PSI))
