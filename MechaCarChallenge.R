636775634065961730451a916217fddc7fc9ec6b
Christopher Scheideman <crscheideman@gmail.com>
2020-11-01 15:08:54 -0500

    updating images

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -10,3 +10,12 @@ total_summary <- Suspension_Coil %>%
   summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD = sd(PSI))
 lot_summary <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>%  
   summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD = sd(PSI))
+
+# Deliverable 3
+t.test(Suspension_Coil$PSI, mu=1500)
+
+t.test(subset(Suspension_Coil, Manufacturing_Lot =='Lot1')$PSI, mu=1500)
+
+t.test(subset(Suspension_Coil, Manufacturing_Lot =='Lot2')$PSI, mu=1500)
+
+t.test(subset(Suspension_Coil, Manufacturing_Lot =='Lot3')$PSI, mu=1500)
